138eee19f82ec5ad8c9aadaf3b28e7ae698001b8
junseopark0331 <junseopark0331@icloud.com>
2023-11-03 23:49:59 +0900

    Project Setting

---
+++ JeongDaeri-Homework1/JeongDaeri-Homework1/SceneDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  SceneDelegate.swift
+//  JeongDaeri-Homework1
+//
+//  Created by 박준서 on 11/3/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene) // SceneDelegate의 프로퍼티에 설정해줌
+        let mainViewController = ViewController() // 맨 처음 보여줄 ViewController
+
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
+
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+       
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+
+
+}
+
